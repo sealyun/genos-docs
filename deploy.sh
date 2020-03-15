ac3b883cbfdfaa44bfad5077b99e1c59c4cd1608
fanux <fhtjob@hotmail.com>
2020-03-15 20:42:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 yarn build
-# git add .
-# git commit -m "update"
-# git push
+git add .
+git commit -m "update"
+git push
 cp -r docs/.vuepress/dist/* ~/work/src/github.com/sealyun/sealyun.github.io
 cd ~/work/src/github.com/sealyun/sealyun.github.io
 git add .
